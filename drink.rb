d64f9499306cd5bb3ef490a33ee6dfb2a71ac3e1
Michael Conner <michaelconner@hotmail.co.uk>
2018-11-28 11:25:38 +0000

    mvp parts 1-2 completed

--- drink.rb
+++ drink.rb
@@ -1,4 +1,15 @@
 class Drink
 
+  attr_reader :drink_name, :drink_price
+
+  def initialize(drink_name, drink_price)
+    @drink_name = drink_name
+    @drink_price = drink_price
+  end
+
+
+
+
+
 
 end
--- specs/drink_specs.rb
+++ specs/drink_specs.rb
@@ -6,7 +6,15 @@ require_relative("../drink")
 class DrinkTest < MiniTest::Test
 
 
+def test_drink_has_name
+  drink = Drink.new("Red Aftershock", 1.00)
+  assert_equal("Red Aftershock", drink.drink_name)
+end
 
+def test_drink_has_price
+  drink = Drink.new("Red Aftershock", 1.00)
+  assert_equal(1.00, drink.drink_price )
+end
 
 
 
