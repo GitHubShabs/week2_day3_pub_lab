6d128106494b0f081698154061141a6e67f86a56
Michael Conner <michaelconner@hotmail.co.uk>
2018-11-28 11:36:01 +0000

    Add mvp section 3 complete

--- specs/customer_specs.rb
+++ specs/customer_specs.rb
@@ -7,7 +7,20 @@ require_relative("../drink")
 class CustomerTest < MiniTest::Test
 
 
+def test_customer_has_name
+  customer = Customer.new("Begbie", 0.00)
+  assert_equal("Begbie", customer.customer_name)
+end
 
+def test_customer_has_wallet__empty
+  customer = Customer.new("Begbie", 0.00)
+  assert_equal(0.00, customer.wallet)
+end
+
+def test_customer_has_wallet__has_money
+  customer = Customer.new("Begbie", 1000.00)
+  assert_equal(1000.00, customer.wallet)
+end
 
 
 
